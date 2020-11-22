ccc7ebe2413bf56d55f4a71a39aa2cadb5090c66
gaboware <gaboware@gmail.com>
2020-11-22 07:28:55 -0500

    add main.py

--- Day1/main.py
+++ Day1/main.py
@@ -1,1 +1,6 @@
 
+print("Welcome to Name My Band!")
+cityname = input("What's the name of the city you grew up in?\n")
+petname = input("What is the name of your pet or any pet name?\n")
+
+print("This is your Band Name! " + cityname + "" + petname)
