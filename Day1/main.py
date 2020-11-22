21cae9fc7374ef7645f45e3a98367fb44fa4c259
gaboware <gaboware@gmail.com>
2020-11-22 07:39:44 -0500

    final coding draft

--- Day1/main.py
+++ Day1/main.py
@@ -1,6 +1,15 @@
+#A simple program that demonstrates working with the print function, 
+#variables and string manipulation
 
+#print function
 print("Welcome to Name My Band!")
+
+#variables
 cityname = input("What's the name of the city you grew up in?\n")
 petname = input("What is the name of your pet or any pet name?\n")
 
-print("This is your Band Name! " + cityname + "" + petname)
+#string manipulation
+print("This is your Band Name: " + cityname + " " + petname)
+
+#bonus: return the total lenth of the Band Name
+print("And your Band Name is " + str(len(cityname) + len(petname)) + " characters long")
