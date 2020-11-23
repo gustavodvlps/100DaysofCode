4237831fe98741452683fe4b921b8533a54faeea
gaboware <gaboware@gmail.com>
2020-11-23 18:56:24 -0500

    Day 2

---
+++ Day2/README.md
@@ -0,0 +1,9 @@
+Day 2 of 100 days of Python Coding. 
+
+Basic TIP calculator
+
+Topics practiced:
+-Primitive Data Types
+-Mathematical Operations
+-Using F strings
+
--- Day2/main.py
+++ Day2/main.py
@@ -5,8 +5,8 @@ bill = float(input("What was the total bill? $ "))
 people = int(input("How many people to split the bill? "))
 percent = int(input("what percentage tip would you like to give? 10, 12, or 15? "))
 
-#tip calculation
+#tip calculation using round function to two decimals
 payforeach = ((bill * percent)/100 + bill)/people
+finalamount = round(payforeach, 2)
 
-#resut rounded out to two decimals
-print("Each person should pay " + str(round(payforeach, 2)))
+print(f"Each person should pay {finalamount}")
